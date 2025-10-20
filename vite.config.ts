2d3db9ec706f395510c2520c2d090a28058fe348
hutao <1165006789@qq.com>
2025-10-20 17:37:16 +0800

    perf:

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,28 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            // reatc 核心依赖项组合
+            if (
+              id.includes("react") ||
+              id.includes("react-dom") ||
+              id.includes("scheduler")
+            ) {
+              return "react-vendor";
+            }
+            // 其他所有依赖项按您的逻辑拆分
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
